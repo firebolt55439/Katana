a43e67e5cd1b3574e846aa6f180a3479c53fbd31
Sumer Kohli <sumer.kohli@berkeley.edu>
2022-01-15 02:35:12 -0800

    majorfeat(frontend): Upgrade to latest packages, fix breaking changes, fix CSS hovering for first-child

--- src/auth-enabled.js
+++ src/auth-enabled.js
@@ -1,29 +1,31 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/analytics";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/analytics";
 
 export const FIREBASE_AUTH_ENABLED = true;
 
-var fireAuth = null, fireDb = null;
-var fireProvider = null, fireAnalytics = null;
+var fireAuth = null,
+  fireDb = null;
+var fireProvider = null,
+  fireAnalytics = null;
 if (FIREBASE_AUTH_ENABLED) {
-	/* NOTE: You can add your own project details here */
-	const firebaseConfig = {
-		apiKey: "AIzaSyCfVD3eqpe8xTm4_xGF9L_aeKEc4vvZ1qw",
-		authDomain: "katana-3e8a4.firebaseapp.com",
-		databaseURL: "https://katana-3e8a4.firebaseio.com",
-		projectId: "katana-3e8a4",
-		storageBucket: "katana-3e8a4.appspot.com",
-		messagingSenderId: "257503960684",
-		appId: "1:257503960684:web:4f78238819a37bbc768249",
-		measurementId: "G-ENF0CYY0CB"
-	};
-	firebase.initializeApp(firebaseConfig);
-	fireAnalytics = firebase.analytics();
-	fireAuth = firebase.auth();
-	fireDb = firebase.firestore();
-	fireProvider = new firebase.auth.GoogleAuthProvider();
+  /* NOTE: You can add your own project details here */
+  const firebaseConfig = {
+    apiKey: "AIzaSyCfVD3eqpe8xTm4_xGF9L_aeKEc4vvZ1qw",
+    authDomain: "katana-3e8a4.firebaseapp.com",
+    databaseURL: "https://katana-3e8a4.firebaseio.com",
+    projectId: "katana-3e8a4",
+    storageBucket: "katana-3e8a4.appspot.com",
+    messagingSenderId: "257503960684",
+    appId: "1:257503960684:web:4f78238819a37bbc768249",
+    measurementId: "G-ENF0CYY0CB",
+  };
+  firebase.initializeApp(firebaseConfig);
+  fireAnalytics = firebase.analytics();
+  fireAuth = firebase.auth();
+  fireDb = firebase.firestore();
+  fireProvider = new firebase.auth.GoogleAuthProvider();
 }
 
 export const auth = fireAuth;
@@ -32,26 +34,26 @@ export const provider = fireProvider;
 export const analytics = fireAnalytics;
 
 export function logEvent(category, action, label) {
-	if (!analytics) return;
-	var params = {};
-	if (action) {
-		category += action;
-	}
-	if (label) {
-		if (typeof label === 'string') {
-			params["label"] = label;
-		} else {
-			for (var key in label) {
-				params[key] = label[key];
-			}
-		}
-	}
-	analytics.logEvent(category, params);
-	console.debug("ga", category, params);
+  if (!analytics) return;
+  var params = {};
+  if (action) {
+    category += action;
+  }
+  if (label) {
+    if (typeof label === "string") {
+      params["label"] = label;
+    } else {
+      for (var key in label) {
+        params[key] = label[key];
+      }
+    }
+  }
+  analytics.logEvent(category, params);
+  console.debug("ga", category, params);
 }
 
 export function logCustomEvent(category, custom) {
-	if (!analytics) return;
-	analytics.logEvent(category, custom);
-	console.debug("ga", category, custom);
+  if (!analytics) return;
+  analytics.logEvent(category, custom);
+  console.debug("ga", category, custom);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,84 @@
+const path = require("path");
+const process = require("process");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-
-   entry: "./src/index.js",
-   output: {
-      filename: "bundle.js",
-   },
-   module: {
-      rules: [
-         {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-               loader: "babel-loader"
-            }
-         },
-         {
-            test: /\.svg$/,
-            exclude: /node_modules/,
-            use: {
-               loader: 'svg-react-loader'
-            }
-
-         },
-         {
-            test: /\.scss$/,
-            use: [
-               {
-                  loader: MiniCssExtractPlugin.loader,
-                  options: {
-                     publicPath: '../'
-                  }
-               },
-               'css-loader',
-               'sass-loader'
-            ]
-         },
-         {
-            test: /\.(gif|png|jpe?g)$/i,
-            use: [
-               'file-loader',
-               {
-                  loader: 'image-webpack-loader',
-                  options: {
-                     bypassOnDebug: true, // webpack@1.x
-                     disable: true, // webpack@2.x and newer
-                  },
-               },
-            ],
-         }
-      ]
-   },
-   node: {
-      fs: "empty"
-   },
-   plugins: [
-      new HtmlWebPackPlugin({
-         template: "./src/index.html",
-         filename: "./index.html"
-      }),
-      new CopyWebpackPlugin([{ from: 'src/static/images', to: 'static/images' }]),
-      new MiniCssExtractPlugin({
-         // Options similar to the same options in webpackOptions.output
-         // both options are optional
-         filename: "main.css"
-      }),
-      new CleanWebpackPlugin(['dist']),
-   ]
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.svg$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "svg-react-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../",
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    fallback: {
+      fs: false,
+    },
+  },
+  output: {
+    /**
+     * With zero configuration,
+     *   clean-webpack-plugin will remove files inside the directory below
+     */
+    path: path.resolve(process.cwd(), "dist"),
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/static/images", to: "static/images" }],
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "main.css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
