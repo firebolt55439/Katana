1d1728d833f219b6c2627a64bfc587296a3c5a9f
Sumer Kohli <sumer.kohli@berkeley.edu>
2022-01-15 03:14:57 -0800

    refactor(header): improved text contrast for description

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import PlayLogo from '../static/images/play-button.svg';
-import AddLogo from '../static/images/add.svg';
+import PlayLogo from "../static/images/play-button.svg";
+import AddLogo from "../static/images/add.svg";
 
-import Modal from '../components/UI/Modal';
-import MovieDetails from '../components/Movie/MovieDetails';
+import Modal from "../components/UI/Modal";
+import MovieDetails from "../components/Movie/MovieDetails";
 
-import { API_KEY } from '../store/actions/index';
-import axios from 'axios';
+import { API_KEY } from "../store/actions/index";
+import axios from "axios";
 
 export default class Header extends Component {
   state = {
-     toggleModal: false,
-     downloadedMovie: null
-  }
+    toggleModal: false,
+    downloadedMovie: null,
+  };
 
   handleToggleModal = () => {
-    const url = "first_air_date" in this.props.movie ? `https://api.themoviedb.org/3/tv/${this.props.movie.id}?api_key=${API_KEY}&language=en-US`
-                                                     : `https://api.themoviedb.org/3/movie/${this.props.movie.id}?api_key=${API_KEY}&language=en-US`;
+    const url =
+      "first_air_date" in this.props.movie
+        ? `https://api.themoviedb.org/3/tv/${this.props.movie.id}?api_key=${API_KEY}&language=en-US`
+        : `https://api.themoviedb.org/3/movie/${this.props.movie.id}?api_key=${API_KEY}&language=en-US`;
     axios
       .get(url)
-      .then(res => {
+      .then((res) => {
         this.setState({ toggleModal: true, downloadedMovie: res.data });
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
-  }
+  };
 
   closeModal = () => {
-     this.setState({ toggleModal: false })
-  }
+    this.setState({ toggleModal: false });
+  };
 
   render() {
     const backgroundStyle = {
       backgroundSize: "cover",
-      backgroundImage: (this.props.movie.backdrop_path ? `url(https://image.tmdb.org/t/p/original/${this.props.movie.backdrop_path})` : ``),
+      backgroundImage: this.props.movie.backdrop_path
+        ? `url(https://image.tmdb.org/t/p/original/${this.props.movie.backdrop_path})`
+        : ``,
       backgroundPosition: "center",
     };
 
@@ -43,8 +47,13 @@ export default class Header extends Component {
       <>
         <header style={backgroundStyle} className="header">
           <div className="header__container">
-            <h1 className="header__container-heading">{this.props.movie.title || this.props.movie.name}</h1>
-            <button onClick={() => this.handleToggleModal()} className="header__container-btnPlay">
+            <h1 className="header__container-heading">
+              {this.props.movie.title || this.props.movie.name}
+            </h1>
+            <button
+              onClick={() => this.handleToggleModal()}
+              className="header__container-btnPlay"
+            >
               <PlayLogo className="header__container-btnMyList-play" />
               Watch
             </button>
@@ -53,16 +62,21 @@ export default class Header extends Component {
               <AddLogo className="header__container-btnMyList-add" />
               My List
             </button>
-            <p className="header__container-overview">{this.props.movie.overview}</p>
+            <p className="header__container-overview">
+              {this.props.movie.overview}
+            </p>
           </div>
           <div className="header--fadeBottom"></div>
         </header>
 
-        <Modal show={this.state.toggleModal}
+        <Modal
+          show={this.state.toggleModal}
           modalClosed={this.closeModal}
-          movie={this.state.downloadedMovie || this.props.movie}>
-
-          <MovieDetails movie={this.state.downloadedMovie || this.props.movie} />
+          movie={this.state.downloadedMovie || this.props.movie}
+        >
+          <MovieDetails
+            movie={this.state.downloadedMovie || this.props.movie}
+          />
         </Modal>
       </>
     );
